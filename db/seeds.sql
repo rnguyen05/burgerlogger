17830e7a3dc7632a15d55746a37587941c1866df
NGOC NGUYEN <ngocnguyen@NGOCsMacPro-234.local>
2018-06-04 20:29:28 -0700

    Modified database schema, js, and handlebars files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,2 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -3,58 +3,33 @@ $(function() {
   $(".change-devoured").on("click", function(event) {
     var id = $(this).data("id");
     var newDevoured = $(this).data("newdevoured");
-
     var newDevouredState = {
       devoured: newDevoured
     };
-
-    // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: newDevouredState
     }).then(
       function() {
         console.log("changed devour to", newDevoured);
-        // Reload the page to get the updated list
         location.reload();
-      }
-    );
+      });
   });
 
   $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
     event.preventDefault();
-
     var newBurger = {
       burger_name: $("#burger").val().trim(),
       devoured: 0
     };
 
-    // Send the POST request.
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
     }).then(
       function() {
         console.log("created new burger");
-        // Reload the page to get the updated list
         location.reload();
-      }
-    );
-  });
-
-  $(".delete-burger").on("click", function(event) {
-    var id = $(this).data("id");
-
-    // Send the DELETE request.
-    $.ajax("/api/burgers/" + id, {
-      type: "DELETE"
-    }).then(
-      function() {
-        console.log("deleted burger", id);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
+    });
   });
 });
