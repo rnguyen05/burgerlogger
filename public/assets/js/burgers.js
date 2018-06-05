b4f5c2eeb0fbbfa9f15022d74ddfea30206bad78
NGOC NGUYEN <ngocnguyen@NGOCsMacPro-234.local>
2018-06-05 11:02:30 -0700

    Modified css, js, handlebars files

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -18,18 +18,18 @@ $(function() {
 
   $(".create-form").on("submit", function(event) {
     event.preventDefault();
-    var newBurger = {
-      burger_name: $("#burger").val().trim(),
-      devoured: 0
-    };
+      var newBurger = {
+        burger_name: $("#burger").val().trim(),
+        devoured: 0
+      };
 
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      function() {
-        console.log("created new burger");
-        location.reload();
-    });
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          location.reload();
+      });
   });
 });
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-// var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080;
 
-var PORT = process.env.PORT;
+//var PORT = process.env.PORT;
 
 var app = express();
 
